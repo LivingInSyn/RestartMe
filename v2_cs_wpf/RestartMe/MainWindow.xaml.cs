b74245c4006ed3570021fee3108c6ad25bf1d720
Jeremy Mill <jeremymill@gmail.com>
2015-09-29 12:57:29 -0400

    moved v1 to new folder, imported v2
    v2 is a move away from python and kivy to wpf and c#

---
+++ v2_cs_wpf/RestartMe/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RestartMe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Restarter restart;
+        public MainWindow()
+        {
+            InitializeComponent();
+            restart = new Restarter();
+        }
+
+        private void Restart_Click(object sender, RoutedEventArgs e)
+        {
+            restart.restart_now();
+        }
+    }
+}
---
+++ v2_cs_wpf/RestartMe/Restarter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestartMe
+{
+    public class Restarter
+    {
+        public void restart_now()
+        {
+            ProcessStartInfo psi = new ProcessStartInfo("shutdown", "/r /t 3");
+            psi.CreateNoWindow = true;
+            psi.UseShellExecute = false;
+            Process.Start(psi);
+        }
+    }
+}
